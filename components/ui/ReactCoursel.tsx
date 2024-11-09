dee67532659ea627982db1d0220a22f29fff4674
HarshKumar0905 <pranshu09051@gmail.com>
2024-11-10 01:08:22 +0530

    Tuning media

--- components/ui/ReactCoursel.tsx
+++ components/ui/ReactCoursel.tsx
@@ -6,20 +6,25 @@ interface Props {
 }
 
 const ReactCoursel = ({mediaFiles} : Props) => {
+
   return (
-    <div className="mt-2 h-56 sm:h-64 xl:h-80 2xl:h-96 rounded-lg border-2 border-gray-600">
+    <div className="mt-2 h-56 xl:h-80 2xl:h-96 rounded-lg border-2 border-gray-600">
       <Carousel slide={false}>
         {mediaFiles?.map((media, index) => (
-            <div className="rounded-lg">
+            <div>
               {media.type === "image" ? (
                 <img
                   src={media?.url}
                   alt="Image Not Found"
+                  className="rounded-lg object-fill mt-2 h-56 xl:h-80 2xl:h-96 m-auto flex justify-center items-center"
                 />
               ) : (
+                <div className="flex justify-center items-center m-auto rounded-lg">
                 <video src={media?.url}
                 controls
+                className="h-56 xl:h-80 2xl:h-96 m-auto flex justify-center items-center rounded-lg"
                 />
+                </div>
               )}
             </div>
         ))}
