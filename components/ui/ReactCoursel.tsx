940ce247230ccf64fc70d53987224013ea9a5f0b
HarshKumar0905 <pranshu09051@gmail.com>
2024-11-13 13:55:14 +0530

    Image Optimization

--- components/ui/ReactCoursel.tsx
+++ components/ui/ReactCoursel.tsx
@@ -1,6 +1,5 @@
 import { MediaFileType } from "@/lib/actions/thread.actions";
 import { Carousel } from "flowbite-react";
-import Image from "next/image";
 
 interface Props {
   mediaFiles : Array<MediaFileType> | null;
@@ -20,13 +19,13 @@ const ReactCoursel = ({mediaFiles} : Props) => {
                   alt="Image Not Found"
                   className="lg:h-56 xl:h-80 2xl:h-96 m-auto flex justify-center items-center rounded-lg
                   border-1.5 border-gray-600"
-                  width={573} height={381} loading="lazy"
+                  width={573} height={381} loading="eager"
                 />
                 </div>
               ) : ( 
                 <div className="flex justify-center items-center m-auto rounded-lg">
                 <video src={media?.url}
-                controls={true}
+                controls={true} preload="auto"
                 className="lg:h-56 xl:h-80 2xl:h-96 m-auto flex justify-center items-center rounded-lg
                border-1.5 border-gray-600"
                 />
